681fd90a1d6cda983b651cb1aca963757d66ed80
Jerry Owusu <65233860+jerryowusu@users.noreply.github.com>
2022-03-02 14:47:59 +0000

    update the main function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
 import './style.css';
+import setData from './modules/post.js';
+import get from './modules/get.js';
+import gameData from './modules/game.js';
+
+window.onload = async () => {
+  get();
+};
+
+const sendScore = document.querySelector('.submit-btn');
+const refresh = document.querySelector('.refresh-btn');
+
+sendScore.onclick = (e) => {
+  const name = document.getElementById('name');
+  const score = document.getElementById('score');
+
+  if (name.value === '' || score.value === '') {
+    e.preventDefault();
+  } else {
+    setData(name.value, score.value);
+    name.value = '';
+    score.value = '';
+    get();
+  }
+};
+
+refresh.onclick = () => {
+  get();
+};
+
+gameData();
